b493874dfce60cbabea8c3c066a5a9b56c9f11f5
Jorge Contreras <jorge.contreras789@gmail.com>
2023-08-28 16:01:33 -0400

    crawl single page with error handling

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('hey')
+const { crawlPage } = require("./crawl");
+
+function main() {
+    if (process.argv.length < 3) {
+        console.log("no website provided");
+        process.exit(1);
+    }
+    if (process.argv.length > 3) {
+        console.log("too many command line args");
+        process.exit(1);
+    }
+
+    const baseUrl = process.argv[2];
+
+    console.log(`starting crawl of ${baseUrl}`);
+    crawlPage(baseUrl);
+}
+
+main();
